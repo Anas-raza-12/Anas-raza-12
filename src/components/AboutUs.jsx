a385f3a9dd6e684ce0fa1c3df126fcc670cab861
Anas-raza-12 <aranasraza123@gmail.com>
2024-09-26 14:32:26 +0500

    some changing

--- src/components/AboutUs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutUs = () => {
-  return (
-    <div className='container'>About Us</div>
-  )
-}
-
-export default AboutUs
--- src/components/portfolio.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const portfolio = () => {
-  return (
-    <div className='container'>Potfolio</div>
-  )
-}
-
-export default portfolio
